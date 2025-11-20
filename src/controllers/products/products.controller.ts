ac9f03286dcec4bf0a29810d1cb8b2ab4ee02a7f
shivamkarn <shivamkarn767@gmail.com>
2025-11-20 12:40:26 +0545

    Added basic postman.json file

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,8 +1,40 @@
-import { Response, Request } from "express";
-const getProductsHandler = async (req: Request, res: Response) => {
+import { asyncHandler } from "../../utils/asyncHandler";
+import { ApiResponse } from "../../utils/ApiResponse";
+
+const getAllProductsHandler = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json({ message: " Here you go all the products served" });
-};
+    .json(new ApiResponse(200, {}, "Here you go all the products served"));
+});
 
-export { getProductsHandler };
+const createProductHandler = asyncHandler(async (req, res) => {
+  return res
+    .status(201)
+    .json(new ApiResponse(201, {}, "Product Created Successfully"));
+});
+
+const updateProductHandler = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Product Updated Successfully"));
+});
+
+const deleteProductHandler = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Product Deleted Successfully"));
+});
+
+const getProductByIdHandler = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Here you go the product by Id"));
+});
+
+export {
+  getAllProductsHandler,
+  createProductHandler,
+  updateProductHandler,
+  deleteProductHandler,
+  getProductByIdHandler,
+};
