aedbdb44433197357245adc3eb4d993ce3a088e1
shivamkarn <shivamkarn767@gmail.com>
2025-11-20 12:40:47 +0545

    Update product router endpoint to include API versioning

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,8 @@ app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
 // products routes
-
 import productRouter from "./routes/products/product.routes";
-app.use("/products", productRouter);
+app.use("/api/v1/products", productRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Server is Running Great", success: true });
