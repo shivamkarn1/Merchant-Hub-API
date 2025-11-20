a923f02aa953493f4add4de1e25f367dc49b0a88
shivamkarn <shivamkarn767@gmail.com>
2025-11-20 12:41:14 +0545

    Product Routes Setup

--- src/routes/products/product.routes.ts
+++ src/routes/products/product.routes.ts
@@ -1,7 +1,19 @@
-import express, { Router } from "express";
-import { getProductsHandler } from "../../controllers/products/products.controller";
+import { Router } from "express";
+import {
+  createProductHandler,
+  getAllProductsHandler,
+  updateProductHandler,
+  deleteProductHandler,
+  getProductByIdHandler,
+} from "../../controllers/products/products.controller";
 const productRouter = Router();
 
-productRouter.get("/allProducts", getProductsHandler);
+// Prefix : /api/v1/products/.....
+
+productRouter.get("/allProducts", getAllProductsHandler);
+productRouter.get("/:id", getProductByIdHandler);
+productRouter.post("/create", createProductHandler);
+productRouter.put("/update/:id", updateProductHandler);
+productRouter.delete("/delete/:id", deleteProductHandler);
 
 export default productRouter;
