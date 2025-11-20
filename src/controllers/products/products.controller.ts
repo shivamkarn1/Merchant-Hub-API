bd4c88ac4f1e13f918f57644cb6f04277fa2dd9f
shivamkarn <shivamkarn767@gmail.com>
2025-11-20 17:21:21 +0545

    Completed Get And Create Products.

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,34 +1,65 @@
 import { asyncHandler } from "../../utils/asyncHandler";
 import { ApiResponse } from "../../utils/ApiResponse";
+import { db } from "../../db/db";
+import { productsTable } from "../../db/products.schema";
+import Status from "../../utils/HttpStatusCodes";
+import { ApiError } from "../../utils/ApiError";
+import createProductSchema from "./product.validation";
+import { createProduct } from "../../services/products/products.service";
+import { zodToApiError } from "../../utils/handleZodError";
 
 const getAllProductsHandler = asyncHandler(async (req, res) => {
+  const products = await db.select().from(productsTable);
   return res
-    .status(200)
-    .json(new ApiResponse(200, {}, "Here you go all the products served"));
+    .status(Status.OK)
+    .json(
+      new ApiResponse(
+        Status.OK,
+        { products },
+        "Here you go all the products served"
+      )
+    );
 });
 
 const createProductHandler = asyncHandler(async (req, res) => {
+  // sabsa pahila ta input validation
+  const parseResult = await createProductSchema.safeParseAsync(req.body);
+  // check kara prlai ki success velai ki nai parsing
+  if (!parseResult.success) {
+    throw zodToApiError(parseResult.error);
+  }
+
+  console.log("The data to be sent for creation is : ", parseResult.data);
+  // okr baad service call kara partai nai
+  const createdProduct = await createProduct(parseResult.data);
+  // and last me return success response
   return res
-    .status(201)
-    .json(new ApiResponse(201, {}, "Product Created Successfully"));
+    .status(Status.Created)
+    .json(
+      new ApiResponse(
+        Status.Created,
+        { createdProduct },
+        "Product Created Successfully"
+      )
+    );
 });
 
 const updateProductHandler = asyncHandler(async (req, res) => {
   return res
-    .status(200)
-    .json(new ApiResponse(200, {}, "Product Updated Successfully"));
+    .status(Status.OK)
+    .json(new ApiResponse(Status.OK, {}, "Product Updated Successfully"));
 });
 
 const deleteProductHandler = asyncHandler(async (req, res) => {
   return res
-    .status(200)
-    .json(new ApiResponse(200, {}, "Product Deleted Successfully"));
+    .status(Status.OK)
+    .json(new ApiResponse(Status.OK, {}, "Product Deleted Successfully"));
 });
 
 const getProductByIdHandler = asyncHandler(async (req, res) => {
   return res
-    .status(200)
-    .json(new ApiResponse(200, {}, "Here you go the product by Id"));
+    .status(Status.OK)
+    .json(new ApiResponse(Status.OK, {}, "Here you go the product by Id"));
 });
 
 export {
